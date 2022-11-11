b4233ef28437339672d336a56a65b0829e6f9263
Nasrin Jahan Ripa <ripanasrin62@gmail.com>
2022-11-11 21:08:13 +0600

    unncessesary file deleted

--- customer/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-            include 'include/connection.php';
-            $pid = 31;
-            $sql2 = "select * from products where p_id = $pid";
-            $result2 = mysqli_query($con,$sql2);
-            $row2 = mysqli_fetch_array($result2);
-            $stored = $row2['quantity'];
-            echo $stored;
-?>
